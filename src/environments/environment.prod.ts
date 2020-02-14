4ced90070b1b20ae763cc164bea73708c7e9c55f
Reginaldo Barros de Sousa <32406036+revelarock@users.noreply.github.com>
2020-02-14 18:25:43 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAK5qlmcWFLL-s5_32nt01kPPL-r5PB8M4",
-    authDomain: "mancell.firebaseapp.com",
-    databaseURL: "https://mancell.firebaseio.com",
-    projectId: "mancell",
-    storageBucket: "mancell.appspot.com",
-    messagingSenderId: "98327534229",
-    appId: "1:98327534229:web:ba540f0172585d0906599c",
-    measurementId: "G-3215HGM2K1"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "ancell.appspot.com",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
